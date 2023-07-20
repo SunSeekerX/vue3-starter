1707583dcef1097816d8e9ca9abfd838a31060f4
SunSeekerX <1647800606@qq.com>
2023-07-21 01:30:04 +0800

    feat: temp

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: ['stylelint-config-recommended-vue'],
+  // extends: ['stylelint-config-recommended-vue'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recommended-vue/scss'],
+  rules: {
+    'no-empty-source': null,
+    'scss/at-rule-no-unknown': null,
+  },
 }
