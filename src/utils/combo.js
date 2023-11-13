d88e0de05617293d6e9124bd8a161c3057d5bc7f
SunSeekerX <1647800606@qq.com>
2023-11-14 02:15:43 +0800

    feat: u

--- src/utils/combo.js
+++ src/utils/combo.js
@@ -3,8 +3,6 @@ import dayjs from 'dayjs'
 import * as api from '@/apis'
 import { i18n } from '@/locales'
 import * as store from '@/store'
-import getEnv from '@/config'
-import * as defaultConfig from '@/config/default'
 import * as constant from '@/constant'
 import * as env from '@/envs'
 
@@ -12,8 +10,6 @@ export default {
   api,
   t: i18n.global.t,
   store,
-  getEnv,
-  defaultConfig,
   constant,
   dayjs,
   i18n,
